1430dff5d1b204cbb805b303fd5147b90bdb7bae
Miguel31218 <miguel.chguillermo@gmail.com>
2024-08-31 17:08:35 -0500

    first commit

---
+++ android/lib/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Prueba Firebase",
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Obtener data",
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
